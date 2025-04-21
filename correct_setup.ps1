147b976fbba4b73ebe2d0a48e6b1df33e3b24cc4
CJ1426 <japanchaiyapat@gmail.com>
2025-04-21 15:19:54 +0700

    update setup script

--- correct_setup.ps1
+++ correct_setup.ps1
@@ -38,4 +38,35 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\P
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Name HideRecommendedSection -Value 1
 
 <#use old context menu#>
-New-Item -Path "HKLM:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InProcServer32" -Force
+New-Item -Path "HKLM:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name InProcServer32 -Value "" -Force
+
+<#ref:https://github.com/ChrisTitusTech/winutil/blob/main/config/tweaks.json#>
+<#disable Activity history#>
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name EnableActivityFeed -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name PublishUserActivities -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name UploadUserActivities -Value 0
+
+<#disable Location Tracking#>
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name Value -Value "Deny"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name SensorPermissionState -Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration" -Name Status -Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name AutoUpdateEnabled -Value 0
+
+<#stop install default game#>
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name DisableWindowsConsumerFeatures -Value 1
+
+<#disable wifi sence#>
+Set-ItemProperty -Path "HMLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name Value -Value 0
+Set-ItemProperty -Path "HMLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name Value -Value 0
+
+<#remove copilot#>
+Set-ItemProperty -Path "HMLM:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Name TurnOffWindowsCopilot -Value 1
+Set-ItemProperty -Path "HMCU:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Name TurnOffWindowsCopilot -Value 1
+dism /online /remove-package /package-name:Microsoft.Windows.Copilot
+
+<#disable recall#>
+Set-ItemProperty -Path "HMLM:\Software\Policies\Microsoft\Windows\WindowsAI" -Name DisableAIDataAnalysis -Value 1
+DISM /Online /Disable-Feature /FeatureName:Recall /Quiet /NoRestart
+
+<#disable stickykeys#>
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name Flags -Value 510
