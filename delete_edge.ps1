a9f57e3401b6dc4f3ca795206ce647c46a960def
CJ1426 <japanchaiyapat@gmail.com>
2025-10-31 09:23:52 +0700

    update delete age script

--- delete_edge.ps1
+++ delete_edge.ps1
@@ -1,3 +1,4 @@
 Remove-Item -path "HKLM:\software\Microsoft\edge"
 Remove-Item -path "C:\Program Files (x86)\Microsoft\Edge"
 Remove-Item -path "C:\Program Files (x86)\Microsoft\EdgeCore"
+Remove-Item -path "C:\Program Files (x86)\Microsoft\EdgeUpdate"
